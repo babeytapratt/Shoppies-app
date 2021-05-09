849d9f7422727d9d2e8164a1e1cf32e02bcc1321
Brian Abeyta-Pratt <babeytapratt@gmail.com>
2021-05-09 15:48:26 -0600

    changed to use hook and started fresh

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,61 @@
-import './App.css';
-import SearchPage from './ShoppiesMovieSearch';
+import React, { useState, useEffect } from 'react';
+import "../App.css";
+import Header from '../Header/Header';
+import Movie from './Movie';
+import Search from './Search';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Shoppies</h1>
-        <SearchPage />
+const MOVIE_API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=10bf1bd2"
 
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const [errorMessage, setErrorMessage] = useState(null);
+
+    useEffect(() => {
+        fetch(MOVIE_API_URL)
+          .then(response => response.json())
+          .then(jsonResponse => {
+            setMovies(jsonResponse.Search);
+            setLoading(false);
+        });
+    }, []);
+
+    const search = searchValue => {
+        setLoading(true);
+        setErrorMessage(null);
+
+    fetch('https://www.omdbapi.com/?s=${searchValue}apikey=10bf1bd2')
+        .then(res => res.json())
+        .then(jsonRes => {
+            if (jsonRes.Res === 'true') {
+                setMovies(jsonRes.Search);
+                setLoading(false);
+            } else {
+                setErrorMessage(json.Res.Error)
+                setLoading(false);
+            }
+        });
+    };
+
+    return (
+        <div className="App">
+            <Header text="Shoppie Nominations" />
+            <Search search={search} />
+            <p className="App-intro">Search for a movie and nominate your favorite movie for a Shoppie</p>
+            <div className="movies">
+                {loading && !errorMessage ? (
+                    <span>loading your movies...</span>
+                ) : errorMessage ? (
+                    <div className="errorMessage">{errorMessage}</div>
+                    ) : (
+                    movies.map((movie, index) => (
+                        <Movie key={'${index}-${movie.Title}'} movie={movie} />
+                    )
+                )
+                )}
+            </div>
+        </div>
+    );
+};
 
 export default App;
--- src/components/ShoppiesNominatedMovies.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const NominatedMovies = props => {
-    const { nominatedTitle } = props.nominated;
-}
